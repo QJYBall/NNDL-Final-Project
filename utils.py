da000a71c444b5abdb3258e0deffb8342c6dfbef
BallBallQ <qjy980811@gmail.com>
2021-06-04 16:52:39 +0800

    Commit Code

---
+++ utils.py
@@ -0,0 +1,114 @@
+import torch
+import numpy as np
+import csv
+
+
+# cutout
+class Cutout(object):
+    """Randomly mask out one or more patches from an image.
+
+    Args:
+        n_holes (int): Number of patches to cut out of each image.
+        length (int): The length (in pixels) of each square patch.
+    """
+    def __init__(self, n_holes, length):
+        self.n_holes = n_holes
+        self.length = length
+
+    def __call__(self, img):
+        """
+        Args:
+            img (Tensor): Tensor image of size (C, H, W).
+        Returns:
+            Tensor: Image with n_holes of dimension length x length cut out of it.
+        """
+        h = img.size(1)
+        w = img.size(2)
+
+        mask = np.ones((h, w), np.float32)
+
+        for n in range(self.n_holes):
+            y = np.random.randint(h)
+            x = np.random.randint(w)
+
+            y1 = np.clip(y - self.length // 2, 0, h)
+            y2 = np.clip(y + self.length // 2, 0, h)
+            x1 = np.clip(x - self.length // 2, 0, w)
+            x2 = np.clip(x + self.length // 2, 0, w)
+
+            mask[y1: y2, x1: x2] = 0.
+
+        mask = torch.from_numpy(mask)
+        mask = mask.expand_as(img)
+        img = img * mask
+
+        return img
+
+
+# cutmix
+"""输入为：样本的size和生成的随机lamda值"""
+def rand_bbox(size, lam):
+    W = size[2]
+    H = size[3]
+    """1.论文里的公式2，求出B的rw,rh"""
+    cut_rat = np.sqrt(1. - lam)
+    cut_w = np.int(W * cut_rat)
+    cut_h = np.int(H * cut_rat)
+ 
+    # uniform
+    """2.论文里的公式2，求出B的rx,ry（bbox的中心点）"""
+    cx = np.random.randint(W)
+    cy = np.random.randint(H)
+    #限制坐标区域不超过样本大小
+ 
+    bbx1 = np.clip(cx - cut_w // 2, 0, W)
+    bby1 = np.clip(cy - cut_h // 2, 0, H)
+    bbx2 = np.clip(cx + cut_w // 2, 0, W)
+    bby2 = np.clip(cy + cut_h // 2, 0, H)
+    """3.返回剪裁B区域的坐标值"""
+    return bbx1, bby1, bbx2, bby2
+
+
+# mixup
+def mixup_data(x, y, alpha=1.0, use_cuda=True):
+    '''Returns mixed inputs, pairs of targets, and lambda'''
+    if alpha > 0:
+        lam = np.random.beta(alpha, alpha)
+    else:
+        lam = 1
+
+    batch_size = x.size()[0]
+    if use_cuda:
+        index = torch.randperm(batch_size).cuda()
+    else:
+        index = torch.randperm(batch_size)
+
+    mixed_x = lam * x + (1 - lam) * x[index, :]
+    y_a, y_b = y, y[index]
+    return mixed_x, y_a, y_b, lam
+
+
+# CSVLogger
+class CSVLogger():
+    def __init__(self, args, fieldnames, filename='log.csv'):
+
+        self.filename = filename
+        self.csv_file = open(filename, 'w')
+
+        # Write model configuration at top of csv
+        writer = csv.writer(self.csv_file)
+        for arg in vars(args):
+            writer.writerow([arg, getattr(args, arg)])
+        writer.writerow([''])
+
+        self.writer = csv.DictWriter(self.csv_file, fieldnames=fieldnames)
+        self.writer.writeheader()
+
+        self.csv_file.flush()
+
+    def writerow(self, row):
+        self.writer.writerow(row)
+        self.csv_file.flush()
+
+    def close(self):
+        self.csv_file.close()
